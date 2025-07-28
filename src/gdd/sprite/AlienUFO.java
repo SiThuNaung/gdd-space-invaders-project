099580b992807015ad62316b76ca1468eb467075
Tommy <thantzinmin.us@gmail.com>
2025-07-28 13:19:20 +0700

    fixed/ zig-zag movement

--- src/gdd/sprite/AlienUFO.java
+++ src/gdd/sprite/AlienUFO.java
@@ -5,17 +5,11 @@ import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.RasterFormatException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import javax.swing.ImageIcon;
 
 public class AlienUFO extends Enemy {
 
-//    private List<Bomb> bombs = new ArrayList<>();
     private int bombCooldown = 0;
-
     private int animationCounter = 0;
     private int currentFrameIndex = 0;
     private final int ANIMATION_DELAY = 5;
@@ -25,6 +19,11 @@ public class AlienUFO extends Enemy {
     private String action = ACT_FLYING;
 
     private int dy = 3;
+
+    // 🟡 Zig-Zag Movement Variables
+    private int dx = 2; // horizontal speed
+    private int zigzagCounter = 0;
+    private final int ZIGZAG_INTERVAL = 60;
 
     private final Rectangle[] flyingFrames = new Rectangle[] {
             new Rectangle(15, 33, 28, 29),
@@ -49,23 +48,16 @@ public class AlienUFO extends Enemy {
 
     public AlienUFO(int x, int y) {
         super(x, y);
-        // bomb = new Bomb(x, y);
         this.type = "AlienUFO";
         initAlienUFO(x, y);
-        // ImageIcon ii = new ImageIcon(IMG_ALIEN_UFO);
-        // setImage(ii.getImage());
-        // currentFrame = flyingFrames[0];
-
     }
 
     public void initAlienUFO(int x, int y) {
         this.x = x;
         this.y = y;
-
         ImageIcon ii = new ImageIcon(IMG_ALIEN_UFO);
         setImage(ii.getImage());
         currentFrame = flyingFrames[0];
-//        bomb = new Bomb(x, y);
     }
 
     @Override
@@ -79,7 +71,6 @@ public class AlienUFO extends Enemy {
         int frameWidth = currentFrame.width;
         int frameHeight = currentFrame.height;
 
-        // Clamp frame dimensions to image bounds to avoid exceptions
         if (frameX + frameWidth > bImage.getWidth()) {
             frameWidth = bImage.getWidth() - frameX;
         }
@@ -90,25 +81,37 @@ public class AlienUFO extends Enemy {
         try {
             return bImage.getSubimage(frameX, frameY, frameWidth, frameHeight);
         } catch (RasterFormatException e) {
-            // Fallback if something still goes wrong
             return bImage;
         }
     }
 
-    // @Override
-    // public void act(int direction) {
-    // super.act(direction);
-    // act();
-    // }
-
+    // 🟢 MODIFIED: Zig-Zag Movement Logic Added Here
     public void act(int direction) {
-        this.y++;
-        act();
-        // Bomb dropping logic
-//        tickCooldown();
-//        if (canDropBomb() && Math.random() < 0.02) { // 2% chance per frame
-//            dropBomb();
-//        }
+        // Zig-zag pattern
+        x += dx;
+        y += dy;
+
+        // Bounce when time or edge is reached
+        zigzagCounter++;
+        if (zigzagCounter >= ZIGZAG_INTERVAL || x <= 0 || x + getWidth() >= BOARD_WIDTH) {
+            dx = -dx;
+            zigzagCounter = 0;
+        }
+
+        act(); // run animation logic
+    }
+
+    @Override
+    public void act() {
+        super.act();
+        animationCounter++;
+
+        if (ACT_FLYING.equals(action)) {
+            if (animationCounter % ANIMATION_DELAY == 0) {
+                currentFrameIndex = (currentFrameIndex + 1) % flyingFrames.length;
+                currentFrame = flyingFrames[currentFrameIndex];
+            }
+        }
     }
 
     @Override
@@ -125,72 +128,4 @@ public class AlienUFO extends Enemy {
     public Rectangle getBounds() {
         return new Rectangle(x, y, getWidth(), getHeight());
     }
-
-    @Override
-    public void act() {
-        super.act();
-        animationCounter++;
-
-        if (ACT_FLYING.equals(action)) {
-            if (animationCounter % ANIMATION_DELAY == 0) {
-                currentFrameIndex = (currentFrameIndex + 1) % flyingFrames.length;
-                currentFrame = flyingFrames[currentFrameIndex];
-            }
-        }
-        // // Drop bombs periodically
-        // if (bombCooldown <= 0) {
-        // dropBomb();
-        // bombCooldown = 120; // Adjust delay between bombs
-        // } else {
-        // bombCooldown--;
-        // }
-
-        // // Update bombs
-        // Iterator<Bomb> iter = bombs.iterator();
-        // while (iter.hasNext()) {
-        // Bomb b = iter.next();
-        // b.act();
-        // if (!b.isVisible()) {
-        // iter.remove();
-        // }
-        // }
-
-    }
-
-    // public void dropBomb() {
-    // bombs.add(new Bomb(x + getWidth() / 2, y + getHeight()));
-    // }
-
-    // public List<Bomb> getBombs() {
-    // return bombs;
-    // }
-
-    // public class Bomb extends Sprite {
-    // private boolean destroyed;
-    // public Bomb(int x, int y) {
-    // this.x = x;
-    // this.y = y;
-    // destroyed = false;
-
-    // var ii = new ImageIcon("src/images/bomb.png");
-    // setImage(ii.getImage());
-    // }
-
-    // @Override
-    // public void act() {
-    // y += 4; // Bomb fall speed
-    // if (y > BOARD_HEIGHT) {
-    // setVisible(false);
-    // }
-    // }
-
-    // public boolean isDestroyed() {
-    // return destroyed;
-    // }
-
-    // public void setDestroyed(boolean val) {
-    // destroyed = val;
-    // }
-    // }
-
 }
